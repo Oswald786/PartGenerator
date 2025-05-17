3bf6047de34c20ae92d2d58b65000e846b06bcab
Oswald786 <ethanslade63@gmail.com>
2025-05-17 20:27:35 +0100

    database and txt file for customer order postman testing all green successfully builds and stores customer order inside txt file need to build testing along with finalising soem details try and make front end look nicer.

--- PartSelectorTool/src/main/java/com/ethan/controller/CustomerController.java
+++ PartSelectorTool/src/main/java/com/ethan/controller/CustomerController.java
@@ -6,13 +6,26 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 
+import java.io.*;
+
 @Controller("/v1Customer")
 
 public class CustomerController {
 
-    @Post("storeOrder")
-    public CustomerOrder storeOrder(@Body CustomerOrder order){
-        //info to store to wherever i would want database etc
-        return order;
+    @Post("/PlaceOrder")
+    public void placeOrder(@Body CustomerOrder order){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter( order.getSurname() + "Order.txt"));
+            writer.write("customer name: " + order.getForename() + " " + order.getSurname());
+            writer.write("\ncustomer Age: " + order.getAge());
+            writer.write("\ncustomer delivery address: " + order.getFullAddress());
+            writer.write("\ncustomer mobile: " + order.getMobileNumber());
+            writer.write("\nPart ordered name: " + order.getPartOrdered().getPartName() + "\nPart msrp: " + order.getPartOrdered().getMsrp() + "\nPart Catagory: " + order.getPartOrdered().getPartCatagory() + "\nPart Tier: " + order.getPartOrdered().getPartTier());
+            writer.close();
+            // could improve by adding marker for where order file is now stored could be sent to database or something similar.
+            System.out.println("order created use path ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
